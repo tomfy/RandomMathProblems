1883b1f98e67939ce7b4b58f8851d65d7c626694
Thomas Fisher-York <tly2@cornell.edu>
2012-11-05 10:10:28 -0500

    Added page_o_problems subroutine.

--- MysteryNumber.pm
+++ MysteryNumber.pm
@@ -3,7 +3,8 @@ use strict;
 use Moose;
 use namespace::autoclean;
 use Carp;
-use List::Util qw' min max sum';
+use List::Util qw ( min max sum );
+use TomfyTex qw ( box_chain answer_box );
 
 use base 'Problem';
 
@@ -28,37 +29,9 @@ has divisors => (
 	       default => sub{ [2,3,4,5,6,7,8,9,10] }
 );
 
-# sub n_digits{
-# 	my $self = shift;
-# 	my $n_digits = shift;
-# 	if(defined $n_digits){
-# 		$self->{n_digits} = $n_digits;
-# 	}
-# 	return $self->{n_digits};
-# }
-
-# sub divisors{
-# 	my $self = shift;
-# 	my $divisors = shift;
-# 	if(defined $divisors){
-# 		$self->{divisors} = $divisors;
-# 	}
-# 	return $self->{divisors};
-# }
 
 sub BUILD{
 my $self = shift;
-	# my $class = shift;
-	# my $self = bless {}, $class;
-	# my $args = shift || {}; # hash ref with args and values
-	# my $default_params = { 'n_digits' => 3, 'divisors' => [2, 3, 4, 5, 10] };
-	# foreach (keys %$default_params){
-	#   $self->{$_} = $default_params->{$_};
-	# }
-	# foreach (keys %$args){
-	#  # print "resetting parameter $_ to ", $args->{$_}, "\n";
-	#   $self->{$_} = $args->{$_};
-	# }
 
 	my $problem_text_template = 'Can you figure out the mystery number? Here are some clues. \newline ' .
 	"It is the EXTREME NDIGITS-digit number which: " .
@@ -83,8 +56,12 @@ sub random_problem{
 	my $divisor ;
 # my %div_count = ();
 # 	for (1..300) {
-	  my $index = int(rand() * scalar @divisors);
+	  my $index = int(rand() * (1 + scalar @divisors));
+	if($index == scalar @divisors){
+	  $divisor = 'odd';
+	}else{
 	  $divisor = $divisors[$index];
+	}
 #	  $div_count{$divisor}++;
 	#  print "divisor: $divisor \n";
 #	}
@@ -98,7 +75,7 @@ sub random_problem{
 		my $candidate_required_digit = int(rand() * 10);
 		$required_digits{$candidate_required_digit}++;
   	}
- 		
+
 	my $problem_text = $self->problem_text_template();
 # print "A $problem_text \n";
 	$problem_text =~ s/EXTREME/$which_extreme/;
@@ -109,7 +86,8 @@ sub random_problem{
 # What number am I thinking of? _______________
 # (The answer is 958, smallest would be 590)
 
-my $other_clues = ' \item is a multiple of ' . $divisor . ' and ';
+my $other_clues = ($divisor eq 'odd')? ' \item is an odd number, and ' : 
+  ' \item is a multiple of ' . $divisor . ' and ';
 for (keys %required_digits){
 $other_clues .= ' \item has at least one digit which is a ' . $_ . ', and ';
 }
@@ -122,6 +100,20 @@ my $answer_text = $self->answer_text_template();
 	return ($problem_text, $answer_text);
 }
 
+sub page_o_problems{
+  my $self = shift;
+  my $n_problems_on_page = shift || 3;	# number of problems on the page.
+  my $problems_string = '';
+ for (1..$n_problems_on_page) {
+    my ($problem, $answer) = $self->random_problem();
+    $problems_string .= '\item ' . $problem . 
+      answer_box('1.5in') . ' \vspace{0.5mm}' . "\n";
+  }
+ my $page_string = $self->page_tex_template();
+$page_string =~ s{PAGE_TITLE}{Mystery Numbers};
+  $page_string =~ s{THE_PROBLEMS}{$problems_string};
+  return $page_string;
+}
 
 
 __PACKAGE__->meta->make_immutable;
