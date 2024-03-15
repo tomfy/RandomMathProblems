e5f9501edfe51ccc86502ae2a961e2d7eeab6be1
tomfy <tly2@cornell.edu>
2024-03-15 14:37:10 -0400

    ---

--- Problem.pm
+++ Problem.pm
@@ -14,7 +14,7 @@ my $page_tex_template =
 #  'BSES Math Team  \\hspace{1in}   
 'PAGE_TITLE  \newline \vspace{4 mm} \newline ' . "\n" .
  ' What is your name? ' . answer_box('3.5in') . ' \newline \vspace{0.5mm} \newline' . "\n" .
-' EXAMPLE \newline ' . # \vspace{4mm} ' .
+# ' EXAMPLE \newline ' . # \vspace{4mm} ' .
   '\begin{enumerate}   [itemsep=2em, topsep=0.3em]  ' . "\n" .
  # '\item What is your name? ' . answer_box('3.5in') . ' \vspace{0.5mm}' . "\n" .
   ' THE_PROBLEMS ' . ' \end{enumerate}  \pagebreak ' . "\n";
